d1914e0e585a03533dff1d4aaa58c17ffaa8a7cf
Alvaro <alvaro.pozo.cuenca@gmail.com>
2018-04-19 20:34:20 +0200

    Config

--- src/main/java/com/adelpozo/spring/bootable/hello/Application.java
+++ src/main/java/com/adelpozo/spring/bootable/hello/Application.java
@@ -1,13 +1,7 @@
 package com.adelpozo.spring.bootable.hello;
 
-import com.adelpozo.spring.configuration.AppStarWarsWebClient;
-import com.adelpozo.spring.configuration.ApplicationConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
