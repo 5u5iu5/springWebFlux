ad7e15ca1c07df9c26335cb33cd8a7dd69fd4d67
Alvaro <alvaro.pozo.cuenca@gmail.com>
2018-04-21 16:15:04 +0200

    Refctor

---
+++ src/main/java/com/apozo/starwars/categorize/kafka/KafkaPublisher.java
@@ -0,0 +1,28 @@
+package com.apozo.starwars.categorize.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class KafkaPublisher {
+
+    public static Logger logger = LoggerFactory.getLogger(KafkaPublisher.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    private final CountDownLatch latch = new CountDownLatch(3);
+
+    public void publish(final String categorizeData, final String topic) throws InterruptedException {
+        this.template.send(topic, categorizeData);
+        latch.await(60, TimeUnit.SECONDS);
+        logger.info("All received");
+    }
+
+}
--- src/main/java/com/apozo/starwars/categorize/service/CategorizeService.java
+++ src/main/java/com/apozo/starwars/categorize/service/CategorizeService.java
@@ -1,5 +1,6 @@
 package com.apozo.starwars.categorize.service;
 
+import com.apozo.starwars.categorize.kafka.KafkaPublisher;
 import com.apozo.starwars.categorize.payload.People;
 import com.apozo.starwars.categorize.webclient.AppStarWarsClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,22 @@ public class CategorizeService {
     @Autowired
     private AppStarWarsClient starWarsClient;
 
+    @Autowired
+    private KafkaPublisher kafkaPublisher;
+
     public Flux<String> getPeopleFromStarWarsWorld() {
         Flux<People> peopleFlux = starWarsClient.listPeopleFromStarWars();
-        Flux<String> resultMap = peopleFlux.map(people -> people != null ? "Starting to publish..." : "Consume from Star Wars Api Failed ");
+        Flux<String> resultMap = peopleFlux.map(people -> {
+            try {
+                if (people != null) {
+                    kafkaPublisher.publish(people.getType(), "jedis");
+                    return "Kafka publisher starting...";
+                } else
+                    return "No data to publish";
+            } catch (InterruptedException e) {
+               return "Several error from Kafka Publisher ";
+            }
+        });
 
         return resultMap;
     }
