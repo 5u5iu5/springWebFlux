88dc6b9f907ba8ad07c9cbe9a513ef60c9c26570
sucius <alvaro.pozo.cuenca@gmail.com>
2018-04-25 16:11:46 +0200

    Reffactor to extrat more pages

--- src/main/java/com/apozo/starwars/categorize/webclient/AppStarWarsClient.java
+++ src/main/java/com/apozo/starwars/categorize/webclient/AppStarWarsClient.java
@@ -2,6 +2,7 @@ package com.apozo.starwars.categorize.webclient;
 
 import com.apozo.starwars.categorize.config.ApplicationConfig;
 import com.apozo.starwars.categorize.payload.ListPeople;
+import com.apozo.starwars.categorize.payload.People;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rajeevkumarsingh on 10/11/17.
@@ -38,16 +42,22 @@ public class AppStarWarsClient {
     }
 
 
-    public Flux<ListPeople> listPeopleFromStarWars(final String page) {
+    private Flux<ListPeople> listPeopleFromStarWars(final String page) {
         return webClient.get()
-                .uri(appProperties.getEndpoint() + page)
+                .uri(appProperties.getEndpoint())
                 .exchange()
                 .flatMapMany(clientResponse -> clientResponse.bodyToFlux(ListPeople.class));
     }
 
     public Flux<ListPeople> listPeopleFromStarWars() {
         Flux<ListPeople> listPeopleFlux = listPeopleFromStarWars("");
-        return listPeopleFlux;
+        Flux<ListPeople> listPeopleFlux1 = listPeopleFlux.flatMap(i -> webClient.get()
+                .uri("/api/people/?page={page}", i.getNext().toString().split("\\?")[1].replace("page=", ""))
+                .retrieve()
+                .bodyToMono(ListPeople.class), 1);
+
+
+        return listPeopleFlux1;
     }
 
 
